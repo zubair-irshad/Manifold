4d5b03b229650837a5e70e4c6e350ab93c833667
Muhammad Zubair Irshad <muhammadzubairirshad@gmail.com>
2023-04-09 19:38:01 -0400

    local changes

--- examples/move_files.py
+++ examples/move_files.py
@@ -5,6 +5,8 @@ import json
 # set the directory path
 dir_path = "/experiments/zubair/shapenet/models"
 
+couch_dir = os.path.join(dir_path, 'couch')
+os.makedirs(couch_dir, exist_ok=True)
 json_path = "/home/ubuntu/zubair/Diffusion-SDF/train_sdf/data/splits/couch_all.json"
 # load the JSON file
 with open(json_path, 'r') as f:
@@ -15,6 +17,6 @@ for subfolder in data['acronym']['Couch']:
     obj_path = os.path.join(dir_path, subfolder + '.obj')
     mtl_path = os.path.join(dir_path, subfolder + '.mtl')
     if os.path.exists(obj_path):
-        shutil.move(obj_path, 'destination_folder')
+        shutil.move(dir_path, 'couch')
     if os.path.exists(mtl_path):
-        shutil.move(mtl_path, 'destination_folder')
+        shutil.move(dir_path, 'couch')
