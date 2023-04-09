83d42e1e31ac31781faf04fedbbc8cac526cdfcc
Muhammad Zubair Irshad <muhammadzubairirshad@gmail.com>
2023-04-09 19:19:00 -0400

    local changes

--- examples/remove_subfolders.py
+++ examples/remove_subfolders.py
@@ -1,9 +1,35 @@
+# import json
+# import os
+
+# # Define the path to the JSON file and the main folder
+# json_path = "/home/zubairirshad/Diffusion-SDF/train_sdf/data/splits/couch_all.json"
+# main_folder = "/home/zubairirshad/Downloads/shapenet/04256520"
+
+
+# # Load the JSON file
+# with open(json_path) as f:
+#     data = json.load(f)
+
+# # Get the list of selected subfolders
+# selected_folders = data["acronym"]["Couch"]
+
+# # Iterate over all subfolders in the main folder
+# for folder in os.listdir(main_folder):
+#     folder_path = os.path.join(main_folder, folder)
+    
+#     # Check if the subfolder is in the list of selected folders
+#     if folder not in selected_folders:
+#         # If not, remove the subfolder and all its contents
+#         os.system("rm -rf {}".format(folder_path))
+
+
+#Remove .obj and .mtl files
 import json
 import os
 
 # Define the path to the JSON file and the main folder
-json_path = "/home/zubairirshad/Diffusion-SDF/train_sdf/data/splits/couch_all.json"
-main_folder = "/home/zubairirshad/Downloads/shapenet/04256520"
+json_path = "/home/ubuntu/Diffusion-SDF/train_sdf/data/splits/couch_all.json"
+main_folder = "/experiments/zubair/shapenet/models"
 
 
 # Load the JSON file
@@ -14,10 +40,10 @@ with open(json_path) as f:
 selected_folders = data["acronym"]["Couch"]
 
 # Iterate over all subfolders in the main folder
-for folder in os.listdir(main_folder):
-    folder_path = os.path.join(main_folder, folder)
+for file in os.listdir(main_folder):
+    name = file[0].split('.')[0]
     
     # Check if the subfolder is in the list of selected folders
-    if folder not in selected_folders:
+    if name not in selected_folders:
         # If not, remove the subfolder and all its contents
-        os.system("rm -rf {}".format(folder_path))
+        os.system("rm -rf {}".format(file))
